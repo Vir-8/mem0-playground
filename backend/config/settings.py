4510662e50041c2ae6935691e3bc497b09f1e85e
Vir Kothari <virkothari8@gmail.com>
2024-10-06 22:50:43 +0530

    Fix Docker config

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['localhost', '0.0.0.0', 'mem0-playground.onrender.com']
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', '.onrender.com']
 
 # Application definition
 
